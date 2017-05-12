48b9b99b22b06cdd737498f11b3ceb7ab31f1934
JunJie Zhou <jzhou@sstparts.com>
2017-05-12 05:54:51 +0000

    create project

---
+++ spaxon-config-service/src/main/java/com/spaxon/configservice/SpaxonConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.spaxon.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaxonConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaxonConfigServiceApplication.class, args);
+	}
+}
