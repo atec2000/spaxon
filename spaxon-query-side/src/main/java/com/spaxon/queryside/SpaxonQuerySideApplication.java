ff6b8eef5cd880e089593f07b350fbb6823870d6
zhou jack <zhou jack>
2017-07-24 20:06:38 +0800

    Add query side

---
+++ spaxon-query-side/src/main/java/com/spaxon/queryside/SpaxonQuerySideApplication.java
@@ -0,0 +1,12 @@
+package com.spaxon.queryside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaxonQuerySideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaxonQuerySideApplication.class, args);
+	}
+}
